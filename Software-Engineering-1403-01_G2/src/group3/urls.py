b5295a973eab051f6116c7efb3d866d37575b5ae
programmer <m_mohamadi_a@yahoo.com>
2025-01-21 12:59:27 +0330

    Added project documentation

---
+++ Software-Engineering-1403-01_G2/src/group3/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'group3'
+urlpatterns = [
+  path('', views.home, name='group3'),
+  path('optimize/', views.optimize_text, name='optimize_text'),
+
+] 
